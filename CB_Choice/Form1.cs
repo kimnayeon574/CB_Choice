9bb81a20e11ce070d818efaa5a4e68f06741f94b
Nayeon Kim <kimn0928@naver.com>
2019-05-14 14:53:06 +0900

    #1 콤보박스 등을 추가하여 폼 디자인 완성

--- CB_Choice/Form1.cs
+++ CB_Choice/Form1.cs
@@ -11,6 +11,11 @@ namespace CB_Choice
 {
     public partial class Form1 : Form
     {
+        // 초기 콤보박스 데이터 설정
+        string[] SList = new string[]{"김밥", "샐러드김밥", "야채김밥",
+            "소고기김밥" ,"계란김밥", "라볶이"};
+        string orgStr = ""; // 선택 결과 저장
+
         public Form1()
         {
             InitializeComponent();
